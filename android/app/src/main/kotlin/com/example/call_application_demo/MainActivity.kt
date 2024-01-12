725f9330156da9d412a6e2c279fc7f8afcd33834
amideo5 <aryantrvd22@gmail.com>
2024-01-13 00:36:10 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/call_application_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.call_application_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:call_application_demo/calculator_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const CalculatorView(),
+    );
+  }
+}
